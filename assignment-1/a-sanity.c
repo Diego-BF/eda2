be74e6858a53c63c3353e64aecabc54c4e57fec5
diego-bf <diegobmfranca@gmail.com>
2019-10-11 14:12:18 -0300

    Add a function to read the dump

--- assignment-1/a-sanity.c
+++ assignment-1/a-sanity.c
@@ -1,44 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void hexa_to_unsigned(char hex_vec[3][9], unsigned* unsig_vec, int pos) {
-  int i;
-
-  for (i = 0; i < 3; i++) {
-    unsig_vec[i] = strtol(hex_vec[i], NULL, 16);
-  }
-}
+#define SIZE_VECT 10000
 
 
-int read_input(unsigned* unsig_vec, int pos) {
+unsigned* read_input() {
   char hex_input[3][9];
-  int ret_scanf = 0;
+  int ret_scanf = 0, i;
+  unsigned *unsig_vec = 0;
 
   ret_scanf = scanf("%8s %8s %8s", hex_input[0], hex_input[1], hex_input[2]);
   if (ret_scanf == 3) {
-    hexa_to_unsigned(hex_input, unsig_vec, pos);
+    // convert hexadecimal to unsigned
+    unsig_vec = malloc(3 * sizeof(unsigned));
+    for (i = 0; i < 3; i++) {
+      unsig_vec[i] = strtol(hex_input[i], NULL, 16);
+    }
   }
 
-  return ret_scanf;
+  return unsig_vec;
+}
+
+
+unsigned** read_dump(int *length) {
+  int i = 0;
+  unsigned **dump = 0, **temp_vect = 0;
+
+  dump = malloc(SIZE_VECT * sizeof(unsigned*));
+
+  dump[0] = read_input();
+  while (dump[*length]) {
+    *length += 1;
+
+    if (*length % SIZE_VECT == 0) {
+      // increase the size of dump
+      temp_vect = dump;
+      dump = malloc((*length + SIZE_VECT) * sizeof(unsigned*));
+
+      for (i = 0; i < *length; i++) {
+        dump[i] = temp_vect[i];
+      }
+      free(temp_vect);
+    }
+    dump[*length] = read_input();
+  }
+
+  return dump;
 }
 
 
 int main() {
-  int i;
-  unsigned ptr1[3], ptr2[3], *dump;
+  int i, length, valid_path = 1;
+  unsigned *ptr1 = 0, *ptr2 = 0, **dump = 0;
 
-  read_input(ptr1, 0);
-  read_input(ptr2, 0);
+  ptr1 = read_input();
+  ptr2 = read_input();
+  dump = read_dump(&length);
 
-  printf("\n\n");
-  for (i = 0; i < 3; i++) {
-    printf("%d ", ptr1[i]);
+  // sort the dump by pointer address
+
+  //check if the path between pointer 1 and 2 is valid
+
+  //check if the path between pointer 2 and 1 (inverse) is valid
+
+  // print if the path is sane
+  if (valid_path) {
+    printf("sana\n");
+  } else {
+    printf("insana\n");
   }
-  printf("\n");
-  for (i = 0; i < 3; i++) {
-    printf("%d ", ptr2[i]);
+
+  // free alocated memory
+  free(ptr1);
+  free(ptr2);
+  for (i = 0; i < length; i++) {
+    free(dump[i]);
   }
-  printf("\n");
+  free(dump);
 
   return 0;
 }
